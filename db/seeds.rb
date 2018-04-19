981c91451b0b9acc8c6639a9242c02d25b7a1385
keiko <keiko.cda@gmail.com>
2018-04-19 17:21:43 +0900

    Fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,3 @@ User.create(
   location: 'Kanagawa, Japan',
   about:    'Hello, I am Ryo. I am from database!'
 )
-User.create(
-  name:     'Shohei Aoki',
-  username: 'moyahima',
-  location: 'Tottori, Japan,
-  about:    'Nice to meet you. I am from database!'
-)

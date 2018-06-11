68338f84f0f0554bf172a8a022269e18d060c47c
keiko <keiko.cda@gmail.com>
2018-06-11 13:31:11 +0900

    Fix

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-class TopController < ApplicationController
-
-def index
-end
-=======
 # frozen_string_literal: true
 
 class TopController < ApplicationController
   def index
   end
->>>>>>> master
 end

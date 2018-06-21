63b522c786b4542e1a29991c24987245a9010261
keiko <keiko.cda@gmail.com>
2018-06-21 16:44:38 +0900

    Change secrets.yml to credential.yml in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,7 @@ set :deploy_to, "/home/deploy/nekotter"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+ append :linked_files, 'config/credebtuaks.yml.enc'
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for default_env is {}

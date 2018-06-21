87b5fd085bc3428b0bbf9e0e64235e751532a9b3
keiko <keiko.cda@gmail.com>
2018-06-21 15:26:23 +0900

    Success SSH:doctor

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,38 +20,38 @@ set :stage, :production
 # Don't use `:all`, it's a meta role.
 
 
-role :web ,%W{deploy@160.16.109.81:26075}
- # role :app, %w{deploy@160.16.109.81:26075}
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/git
-# Feel free to add new variables to customise your setup.
-
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+# role :web ,%W{deploy@160.16.109.81:26075}
+#  # role :app, %w{deploy@160.16.109.81:26075}
+# # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+# # role :db,  %w{deploy@example.com}
 #
-# Global options
-# --------------
-  set :ssh_options, {
-    keys: %w(/Users/keiko/.ssh/client_rsa),
-    port: 26075,
-    # forward_agent: true,
- #    # auth_methods: false,
-  }
 #
+#
+# # Configuration
+# # =============
+# # You can set any configuration variable like in config/deploy.rb
+# # These variables are then only loaded and set in this stage.
+# # For available Capistrano configuration variables see the documentation page.
+# # http://capistranorb.com/documentation/getting-started/configuration/git
+# # Feel free to add new variables to customise your setup.
+#
+#
+#
+# # Custom SSH Options
+# # ==================
+# # You may pass any option but keep in mind that net/ssh understands a
+# # limited set of options, consult the Net::SSH documentation.
+# # http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+# #
+# # Global options
+# # --------------
+#   set :ssh_options, {
+#     keys: %w(/Users/keiko/.ssh/client_rsa),
+#     port: 26075,
+#     # forward_agent: true,
+#  #    # auth_methods: false,
+#   }
+# #
 # The server-based syntax can be used to override options:
 # server "nekotter.tk",
 # user: "keiko",
